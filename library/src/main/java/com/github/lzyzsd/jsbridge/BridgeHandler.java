6b289b4386408f0c659fa8ce35c046d52dc8c675
MartinHY <569835826@qq.com>
2018-10-25 17:06:07 +0800

    Feat: 扩展单一回调的方式

--- library/src/main/java/com/github/lzyzsd/jsbridge/BridgeHandler.java
+++ library/src/main/java/com/github/lzyzsd/jsbridge/BridgeHandler.java
@@ -2,6 +2,6 @@ package com.github.lzyzsd.jsbridge;
 
 public interface BridgeHandler {
 	
-	void handler(String data, CallBackFunction function);
+	void handler(String name ,String data, CallBackFunction function);
 
 }
--- library/src/main/java/com/github/lzyzsd/jsbridge/DefaultHandler.java
+++ library/src/main/java/com/github/lzyzsd/jsbridge/DefaultHandler.java
@@ -3,12 +3,11 @@ package com.github.lzyzsd.jsbridge;
 public class DefaultHandler implements BridgeHandler{
 
 	String TAG = "DefaultHandler";
-	
+
 	@Override
-	public void handler(String data, CallBackFunction function) {
+	public void handler(String name, String data, CallBackFunction function) {
 		if(function != null){
 			function.onCallBack("DefaultHandler response data");
 		}
 	}
-
 }
